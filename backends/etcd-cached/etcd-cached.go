3d0b682f358af17c16a23301f4a881e3e8f33b98
Johnyang2017 <120801192@qq.com>
2018-10-02 08:46:40 +0800

    Modify license part

--- backends/etcd-cached/etcd-cached.go
+++ backends/etcd-cached/etcd-cached.go
@@ -1,4 +1,7 @@
-// Copyright (c) 2017 The containerdns Authors. All rights reserved.
+// Copyright (c) 2014 The SkyDNS Authors.
+// Copyright (c) 2017 The containerdns Authors.
+// Use of this source code is governed by The MIT License (MIT) that can be
+// found in the LICENSE file.
 
 package etcdCached
 
